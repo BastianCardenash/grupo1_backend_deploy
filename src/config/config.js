8138d71c41f3264beaa7251f556b8ece292e3c32
BastianCardenash <bastiancardenash@uc.cl>
2023-12-03 16:20:30 -0300

    configjs

--- src/config/config.js
+++ src/config/config.js
@@ -6,22 +6,22 @@ module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": `${process.env.DB_NAME}_development`,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": `${process.env.DB_NAME}_test`,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": `${process.env.DB_NAME}_production`,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  }
+    "database": process.env.DB_NAME
+  },
+  "host": process.env.DB_HOST,
+  "dialect": "postgres"
 }
